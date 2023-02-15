92f7c95b0437acf396f682505daa281aa41107ab
Ricardo Meneses Morales <ricardomeneses0503@gmail.com>
2023-02-15 17:10:59 -0600

    updated.

--- src/components/utils/Button.tsx
+++ src/components/utils/Button.tsx
@@ -14,7 +14,7 @@ const Button: React.FC<Props> = ({ text, haveIcon, icon }) => {
       href='/CV.pdf'
       target='_blank'
       download
-      className='rounded-[29px] bg-[#1F2A42] text-white px-10 py-2 text-1xl drop-shadow-[-10px_10px_3px_rgba(0,0,0,0.25)] '
+      className='rounded-[29px] bg-[#1F2A42] text-white px-10 py-2 text-1xl drop-shadow-[-10px_10px_3px_rgba(0,0,0,0.25)] flex w-3/4 md:w-2/4 justify-center mx-auto md:mx-0 items-center'
     >
       {text}
       {haveIcon && icon && <FontAwesomeIcon icon={icon} className='ml-2' />}
