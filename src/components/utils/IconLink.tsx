c3106824dbee5c46c07ca7bff53f2389da5912c6
Ricardo Meneses Morales <ricardomeneses0503@gmail.com>
2023-02-15 20:36:51 -0600

    Updated.

--- src/components/utils/IconLink.tsx
+++ src/components/utils/IconLink.tsx
@@ -10,7 +10,7 @@ interface Props {
 const IconLink: React.FC<Props> = ({ icon, href }) => {
   return (
     <Link
-      className='rounded-[50%] w-[50px] h-[50px] bg-[#1F2A42] flex items-center justify-center text-cyan-500 cursor-pointer drop-shadow-[-2px_10px_3px_rgba(0,0,0,0.25)]'
+      className='rounded-[50%] w-[50px] h-[50px] bg-[#1F2A42] flex items-center justify-center text-cyan-500 cursor-pointer'
       href={href}
       target='_blank'
     >
