6cd3af5827fbebecf3bf7defd9743abda5e64661
Ricardo Meneses Morales <ricardomeneses0503@gmail.com>
2023-02-18 00:11:40 -0600

    Changed images.

--- src/components/utils/IconLink.tsx
+++ src/components/utils/IconLink.tsx
@@ -13,6 +13,7 @@ const IconLink: React.FC<Props> = ({ icon, href }) => {
       className='rounded-[50%] w-[50px] h-[50px] bg-gradient-to-r from-cyan-500 to-blue-500 flex items-center justify-center text-white hover:from-white hover:to-white hover:text-cyan-500 hover:scale-110 transition-all duration-200 cursor-pointer'
       href={href}
       target='_blank'
+      aria-label='social-link'
     >
       <FontAwesomeIcon icon={icon} className='text-2xl' />
     </Link>
--- src/pages/proyectos.tsx
+++ src/pages/proyectos.tsx
@@ -10,37 +10,37 @@ const Proyectos = () => {
     {
       title: 'RaccoonDev Studio',
       link: 'https://raccoondev.studio/',
-      img: '/img/RaccoonDev.png',
+      img: '/img/RaccoonDev.webp',
       tech: ['NextJS', 'ReactJS', 'NodeJS', 'Bootstrap'],
     },
     {
       title: 'Oleum Vitae',
       link: 'https://oleumvitae.com.mx/',
-      img: '/img/OleumVitae.png',
+      img: '/img/OleumVitae.webp',
       tech: ['NuxtJS', 'VueJS', 'Shopify', 'NodeJS', 'Bootstrap'],
     },
     {
       title: 'Qveggie',
       link: 'https://www.qveggie.com/',
-      img: '/img/QVeggie.png',
+      img: '/img/QVeggie.webp',
       tech: ['NuxtJS', 'VueJS', 'Worpdress', 'NodeJS', 'Vuetify'],
     },
     {
       title: 'Crepini',
       link: 'https://crepiniplantbased.com/',
-      img: '/img/Crepini.png',
+      img: '/img/Crepini.webp',
       tech: ['NuxtJS', 'VueJS', 'Worpdress', 'NodeJS', 'Vuetify'],
     },
     {
       title: 'Deliver México',
       link: 'https://delivermexico.com.mx/',
-      img: '/img/Deliver.png',
+      img: '/img/Deliver.webp',
       tech: ['NuxtJS', 'VueJS', 'GraphQl', 'Bootstrap'],
     },
     {
       title: 'Colectivo Ahuejote',
       link: 'https://www.colectivoahuejote.mx/',
-      img: '/img/Ahuejote.png',
+      img: '/img/Ahuejote.webp',
       tech: ['NuxtJS', 'VueJS', 'Worpdress', 'NodeJS', 'Vuetify'],
     },
   ];
